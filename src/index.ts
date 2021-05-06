4d338ea3a2f7790eedc2c1b855a5c3803bce4d5e
m.tutynina <m.tutynina@list.ru>
2021-05-06 18:57:38 +0600

    Add defaults override.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 ﻿import { registerPaginationComponent } from "./pagination";
 
 export { registerPaginationComponent } from "./pagination";
+export { overrideConfiguration } from "./shared/config";
 
 export function registerComponents(): void {
     registerPaginationComponent();
