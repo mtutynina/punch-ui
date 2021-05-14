59ad29320410e5424efb6021f41ac56e5ba47261
m.tutynina <m.tutynina@list.ru>
2021-05-14 17:56:18 +0600

    Make configuration override as partial class.

--- src/shared/config/pagination.ts
+++ src/shared/config/pagination.ts
@@ -5,76 +5,7 @@
 
 export type PaginationSize = "default" | "small";
 
-export interface PaginationConfigurationOverride {
-    // eslint-disable-next-line lines-around-comment
-    /**
-     * Specify the sizeChanger options.
-     * Default: [10, 20, 30, 40, 50].
-     */
-    pageSizeOptions?: number[];
-
-    /**
-     * Number of data items per page
-     * Default: 10.
-     * Note: default value should exist in pageSizeOptions list.
-     */
-    pageSize?: number;
-
-    /**
-     * Indicates whether should take page size.
-     * Default: false.
-     */
-    useCookieForPageSize?: boolean;
-
-    /**
-     * Cookie name for page size. Available when useCookieForPageSize is set to true.
-     * Default: pageSize.
-     */
-    pageSizeCookieName?: string;
-
-    /**
-     * Indicates whether should get pageSize and currentPage values from query strings, and override it on change.
-     * Default: false.
-     */
-    useQueryStringParameters?: boolean;
-
-    /**
-     * Default initial value for current page.
-     * Default: 1.
-     */
-    initialCurrentPage?: number;
-
-    /**
-     * Query sting parameter name for current page.
-     * Default: currentPage.
-     */
-    currentPageQueryString?: string;
-
-    /**
-     * Query sting parameter name for page size.
-     * Default: pageSize.
-     */
-    pageSizeQueryString?: string;
-
-    /**
-     * Icon class names for previous and next buttons.
-     * Default: not set.
-     */
-    icons?: PaginationIcons;
-
-    /**
-     * Pagination component size.
-     * Values: default or small.
-     * Default: default.
-     */
-    size?: PaginationSize;
-
-    /**
-     * Count of buttons to switch pages.
-     * Default: 5.
-     */
-    visiblePagesCount?: number;
-}
+export type PaginationConfigurationOverride = Partial<PaginationConfiguration>;
 
 export interface PaginationConfiguration {
     // eslint-disable-next-line lines-around-comment
